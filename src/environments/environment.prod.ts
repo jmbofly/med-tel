149d594aea061fd8bae767323ca2bd1644b9e47f
unknown <flynn-3199@cotc.edu>
2019-05-28 04:57:42 -0400

    modified

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyA3e2c4NkrH3xPq5evKdvddHYl2WOYbeIQ',
+    authDomain: 'medtelplus.firebaseapp.com',
+    databaseURL: 'https://medtelplus.firebaseio.com',
+    projectId: 'medtelplus',
+    storageBucket: 'medtelplus.appspot.com',
+    messagingSenderId: '1081302408278',
+    appId: '1:1081302408278:web:0cd5b9c47e7c90d0',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyA3e2c4NkrH3xPq5evKdvddHYl2WOYbeIQ',
+    authDomain: 'medtelplus.firebaseapp.com',
+    databaseURL: 'https://medtelplus.firebaseio.com',
+    projectId: 'medtelplus',
+    storageBucket: 'medtelplus.appspot.com',
+    messagingSenderId: '1081302408278',
+    appId: '1:1081302408278:web:0cd5b9c47e7c90d0',
+  },
 };
 
 /*
