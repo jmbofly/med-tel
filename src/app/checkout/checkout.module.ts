7d305d63fd105d5d6486e7b798ce9c03355ecd1f
unknown <flynn-3199@cotc.edu>
2019-06-03 22:42:10 -0400

    added checkout component

---
+++ src/app/checkout/checkout.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CheckoutComponent } from './checkout.component';
+
+@NgModule({
+  declarations: [CheckoutComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class CheckoutModule { }
