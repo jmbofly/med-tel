41d85539d9c5a6649b75eb31bcd37790f211d0b9
unknown <flynn-3199@cotc.edu>
2019-06-03 22:44:01 -0400

    modified navbar component

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgbModal, NgbDropdown } from '@ng-bootstrap/ng-bootstrap';
+import { AuthService } from '../core/auth.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
@@ -6,14 +10,72 @@ import { Component, OnInit, ViewChild } from '@angular/core';
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
-  @ViewChild('navbar') nav;
   mobileMenu = true;
-  constructor() {}
+  loginMenu = false;
+  loggedIn: Observable<boolean>;
+  status: string;
+  constructor(
+    private authService: AuthService,
+    private modalService: NgbModal,
+    private router: Router
+  ) {}
+
+  async createAccount(name: string, email: string, password: string) {
+    return this.authService
+      .createUserWithEmailAndPassword(name, email, password)
+      .then(response => {
+        this.loginMenu = false;
+        this.modalService.dismissAll();
+        this.router.navigateByUrl('/store');
+        console.log('registration response', response);
+      })
+      .catch(err => console.log('error creating new account', err));
+  }
+
+  async login(email: string, password: string) {
+    return this.authService
+      .loginWithEmail(email, password)
+      .then(response => {
+        this.loginMenu = false;
+        this.modalService.dismissAll();
+        this.router.navigateByUrl('/store');
+        console.log('login response', response);
+      })
+      .catch(err => console.log('error logging in', err));
+  }
+
+  openLoginModal(content: TemplateRef<any>, setStatus: string) {
+    this.status = setStatus;
+    const modalRef = this.modalService.open(content, {
+      ariaLabelledBy: 'modal-login-title',
+    });
+    modalRef.result.then(results => console.log('modal results', results));
+  }
+
+  async signOut() {
+    if (this.mobileMenu) {
+      this.toggleMobileMenu();
+    }
+    return this.authService
+      .signOut()
+      .then(res => this.router.navigateByUrl('/'));
+  }
+
+  toggleLoginMenu() {
+    this.loginMenu = !this.loginMenu;
+  }
 
   toggleMobileMenu() {
     this.mobileMenu = !this.mobileMenu;
   }
+
   ngOnInit() {
+    this.authService.loggedIn().subscribe(res => {
+      this.loggedIn = new Observable(obs => {
+        obs.next(res !== null);
+      });
+    });
+
     // console.log('nav', this.nav);
   }
 }
